207205e49f82006540e7806b4ffd2f863ddb4318
lizKimita <kimita.wanjiku@gmail.com>
2019-02-19 17:13:46 +0300

    adding DOM manipulation changing hello to green

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why Hello there!</li>");
+    $("li").css("background-color", "green");
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense</li>");
+  });
+
+});
